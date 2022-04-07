829ad9dec5dfa0a3756d7275659b478cf0ff3fdd
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-04-07 12:45:54 -0700

    commit for 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/*
- * main - entry point
- * Return: 0 after printing the function
+/**
+ *main - entry point
+ *
+ *Return: 0 after printing the function
  */
 int main(void)
 {
