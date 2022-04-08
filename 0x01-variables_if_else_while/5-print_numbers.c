78a32d14be659a69a7d63ab916f80bbb63c07831
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-04-08 12:27:07 -0700

    my commit message for 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#inclue <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 Always
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+{
+printf("%n", n);
+}
+
+printf("\n");
+return (0);
+}
