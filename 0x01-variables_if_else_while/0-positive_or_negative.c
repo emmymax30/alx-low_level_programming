7fbf9c83737bc5c729de6ab41999fe25e22d1fb8
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-04-08 09:11:33 -0700

    my commit message for 0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * return: 0 always
+ */
+int main(void)
+{
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n==0)
+{
+printf("%i is zero\n", n);
+}
+else if (n<0)
+printf("%i is negative\n", n);
+}
+else
+{
+printf("%i is positive\n", n);
+}
+
+return (0);
+
+}
