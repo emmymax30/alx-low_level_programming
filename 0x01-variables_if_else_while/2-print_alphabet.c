95a945260ec6e855b59783e3573bb7be04f3d5a7
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-04-08 10:42:13 -0700

    my commit message for 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+ * main - entry point
+ *
+ * Retun: 0 always
+ */
+
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+
+putchar("\n");
+
+return (0);
+}
+
