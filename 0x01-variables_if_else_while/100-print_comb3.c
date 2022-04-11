54386fa00227326b7e709608397404d8c9a993c9
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-04-11 12:44:26 -0700

    task 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 /**
-
- * main - Prints numbers between 00 to 89.
-
+ *main - Prints numbers between 00 to 89.
  *
-
- * Return: Always 0 (Success)
-
+ *Return: Always 0 (Success)
  */
 
 int main(void)
