9d76e739801801c32afc27b8dcab59833e98fa7d
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-04-12 11:44:29 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * return: 0 Always (Success)
+ */
+int main
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
+return (0);
+}
