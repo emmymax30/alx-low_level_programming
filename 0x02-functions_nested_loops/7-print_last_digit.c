612e37db8de1058863dcf5a4cc22168ff3abffa2
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-04-12 14:58:44 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,11 +13,11 @@ if (n < 0)
 last_digit = (-1 * (n % 10));
 _putchar (last_digit + '0');
 return (last_digit);
-
 }
+
 else
 {
-last_digit (n % 10);
+last_digit = (n % 10);
 _putchar (last_digit + '0');
 return (last_digit);
 }
