b413e899f4495c03629399d6c757ab65a4654df0
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-04-12 15:11:14 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,8 +12,8 @@ int b;
 for (a = 0; a <= 23; a++)
 {
 for (b = 0; b <= 59; b++)
-}
 
+{
 _putchar (a / 10 + '0');
 
 _putchar (a % 10 + '0');
@@ -26,4 +26,6 @@ _putchar (b % 10 + '0');
 
 _putchar ('\n');
 }
+}
+
 }
