5ca8a4e9b6da18a56e10b54b1f6ee0e9a1c76b06
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-04-12 14:15:42 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - retun 1 if c is letter, uppercase or lowercase
+ *
+ * Return: 0 Always
+ */
+int _isalpha(int c);
+{
+if (c > 'a' && c < 'z') || (c > 'A' && c < 'Z')
+{
+returm (1);
+}
+else
+{
+returm (0);
+}
+}
