3d0a7b2079fba87837ecf62f93a2eb3c56a34b6c
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-04-14 07:08:12 -0700

    my commit message

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,8 @@
-#include "main.h"
+#include "main.c"
 /**
- * main - Entry
- * return: 0
+ * main - main
+ * Return: 0
  */
-
 int main(void)
 {
 int i;
@@ -12,4 +11,5 @@ i = 0;
 positive_or_negative(i);
 
 return (0);
+
 }
