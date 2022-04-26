64a363a8339cf5575154dbc558ab1f2a45193425
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-04-26 11:26:27 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,8 +1,8 @@
-#include "main.h
+#include "main.h"
 /**
- *_strchr - Entry point
- *@s: input
- *@c: input
+ * _strchr - Entry point
+ * @s: input
+ * @c: input
  * Return: Always 0 (Success)
  */
 char *_strchr(char *s, char c)
