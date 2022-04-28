45c267a827f38794f6cb2dace2332a7ec15170e7
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-04-28 11:50:53 -0700

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
-/*
- * _print_rev_recursion - main - check all the code
+/**
+ * _print_rev_recursion - main - check the code
  * @s: character of a string
- * Return: Always o.
+ * Return: Always 0.
  */
 void _print_rev_recursion(char *s)
 {
