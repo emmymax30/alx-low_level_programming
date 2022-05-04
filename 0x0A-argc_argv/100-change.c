6a935da07209737db2593236135db8e017452d58
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-05-04 05:07:42 -0700

    done

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - prints the function
  * @argc: counts the arguments of the function.
  * @argv: gives the value of the arguments passed to the function.
+ *
  * Return: Return always success.
- */
+**/
+
 int main(int argc, char *argv[])
 {
-
 int cents, coins = 0;
 
 if (argc == 2)
