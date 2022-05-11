a716d3997ca2f99c00105d664ce0e11e3c4e8cc5
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-05-11 20:21:10 +0000

    2

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,9 +1,10 @@
-#define 1_PI
-#ifndef 1_PI
-/**
+#ifndef PI_H
+#define PI_H
+
+/*
  * File: 1-pi.h
  * Desc: Header file that defines a macro named PI
- *
+ *       as an abbreviation for the token 3.14159265359.
  */
 
 #define PI 3.14159265359
