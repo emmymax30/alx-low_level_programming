33725f67bf920480ddff649567948b2127dc11c5
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-07-12 23:55:53 +0000

    .

--- 0x18-dynamic_libraries/5func.c
+++ 0x18-dynamic_libraries/5func.c
@@ -24,15 +24,15 @@ int mod(int a, int b)
 void main()
 {
 	int a = 5, b = 5;
-	int c,d,e;
-	a = sum(a, b);
-	b = div(a, b);
+	int c,d,e,f,g;
+	f = sum(a, b);
+	g = div(a, b);
 	c = mul(a, b);
 	d = sub(a, b);
 	e = mod(a, b);
 
-	printf("Sum = %d\n", a);
-	printf("Division = %d\n", b);
+	printf("Sum = %d\n", f);
+	printf("Division = %d\n", g);
 	printf("Multiplication = %d\n", c);
 	printf("Subtraction = %d\n", d);
 	printf("Mod = %d\n", e);
