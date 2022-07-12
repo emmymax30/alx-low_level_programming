64f8ed119644f8a5f5f38869a5fcf08d42dcb999
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-07-12 23:48:56 +0000

    .

---
+++ 0x18-dynamic_libraries/5func.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int sum(int a, int b)
+{
+	return a+b;
+}
+int div(int a, int b)
+{
+        return a/b;
+}
+int mul(int a, int b)
+{
+        return a*b;
+}
+int sub(int a, int b)
+{
+        return a-b;
+}
+int mod(int a, int b)
+{
+        return a%b;
+}
+
+void main()
+{
+	int a = 5, b = 5;
+	int c,d,e;
+	a = sum(a, b);
+	b = div(a, b);
+	c = mul(a, b);
+	d = sub(a, b);
+	e = mod(a, b);
+
+	printf("Sum = %d\n", a);
+	printf("Division = %d\n", b);
+	printf("Multiplication = %d\n", c);
+	printf("Subtraction = %d\n", d);
+	printf("Mod = %d\n", e);
+}
