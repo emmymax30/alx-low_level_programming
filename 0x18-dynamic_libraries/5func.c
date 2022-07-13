d496674e7c04a06b0f06011abc73c128695e89e6
emmymax30 <Holabodeemmanuel30@gmail.com>
2022-07-13 00:21:39 +0000

    .

--- 0x18-dynamic_libraries/5func.c
+++ 0x18-dynamic_libraries/5func.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int a, b;
+
 int sum(int a, int b)
 {
 	return a+b;
@@ -21,19 +23,3 @@ int mod(int a, int b)
         return a%b;
 }
 
-void main()
-{
-	int a = 5, b = 5;
-	int c,d,e,f,g;
-	f = sum(a, b);
-	g = div(a, b);
-	c = mul(a, b);
-	d = sub(a, b);
-	e = mod(a, b);
-
-	printf("Sum = %d\n", f);
-	printf("Division = %d\n", g);
-	printf("Multiplication = %d\n", c);
-	printf("Subtraction = %d\n", d);
-	printf("Mod = %d\n", e);
-}
---
+++ 0x18-dynamic_libraries/python.h
@@ -0,0 +1,11 @@
+#ifndef DYNAMIC_LIB_H
+#define DYNAMIC_LIB_H
+
+#include <stdlib.h>
+int add(int, int);
+int sub(int, int);
+int mul(int, int);
+int div(int, int);
+int mod(int, int);
+
+#endif
